84158cdbfb0bacf6a9efdf81c6a84d2b80330135
juninho <hei.juninho.2@gmail.com>
2026-02-02 21:07:11 +0300

    Initial project using tailwind css with shadcn ui

---
+++ client/src/App.tsx
@@ -0,0 +1,17 @@
+import { Button } from "./components/ui/button"
+
+
+
+function App() {
+  
+
+  return (
+    <div>
+   <h1 className="text-5xl text-center">hello world</h1>
+   <Button className="m-4 bg-red-500 text-white">Click me</Button>
+ 
+    </div>
+  )
+}
+
+export default App
