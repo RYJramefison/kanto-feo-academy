6c9177a2ef6c1489d55e3e288c9346577b07c1e5
juninho <hei.juninho.2@gmail.com>
2026-02-14 21:35:53 +0300

    fix: update route to navigate to dashboard

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,20 +1,51 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { createBrowserRouter } from 'react-router'
-import About from './pages/About.tsx'
-import Instruments from './pages/Instruments.tsx'
-import TestimonialsSection from './pages/TestimonialsSection.tsx'
 import { RouterProvider } from 'react-router-dom'
-import Contact from './pages/Contact.tsx'
-import { Home } from 'lucide-react'
+import Register from './pages/Register.tsx'
+import Login from './pages/Login.tsx'
+import Dashboard from './pages/dashboard/Dashboard.tsx'
+import DashboardCourses from './pages/dashboard/DashboardCourses.tsx'
+import DashboardPayments from './pages/dashboard/DashboardPayment.tsx'
+import DashboardCalendar from './pages/dashboard/DashboardCalendar.tsx'
+import DashboardProgress from './pages/dashboard/DashboardProgress.tsx'
 
 const router = createBrowserRouter([
   { 
     path: '/', 
     element: <App />,
   },
+  {
+    path: '/register',
+    element: <Register />
+  },
+  {
+    path: '/login',
+    element: <Login />
+  },
+  {
+    path: '/dashboard/',
+    element: <Dashboard />,
+  
+  },
+  {
+    path: '/dashboard/courses',
+    element: <DashboardCourses />,
+  },
+  {
+    path: '/dashboard/payments',
+    element: <DashboardPayments />,
+  },
+  {
+    path: '/dashboard/calendar',
+    element: <DashboardCalendar />,
+  },
+  {
+    path: '/dashboard/progress',
+    element: <DashboardProgress />,
+  },
+
  
 ])
 
